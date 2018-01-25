33aaa25df35ee277fb6f3478e269702da80730b0
mchegondi <mchegondi@USSCCMCHEGOND02>
2018-01-25 00:35:45 -0800

    Adding Vprequest

---
+++ src/PFML.Shared/ViewModels/Premium/Waiver/VPRequest/VPRequest.cs
@@ -0,0 +1,45 @@
+﻿using PFML.Shared.Model.DbDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFML.Shared.ViewModels.Premium.Waiver
+{
+	[Serializable]
+	public class VPRequestViewModel
+	{
+		public int RequestId { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public string StatusCode { get; set; }
+        public string TypesofLeaveAvailable { get; set; }
+        public string WeeksAvailableAnnually { get; set; }
+        public Decimal? PercentageofWagesPaid { get; set; }
+        public string DocumentName { get; set; }
+    }
+
+	[Serializable]
+	public class VPRequestForm
+	{
+		public int EmployerId { get; set; }
+		public DateTime StartDate { get; set; }
+		public DateTime EndDate { get; set; }
+		public List<VoluntaryPlanWaiverRequestTypeDto> RequestType { get; set; }
+		public List<DocumentDto> Document { get; set; }
+		public bool IsAcknolwedged { get; set; }
+	}
+	
+	[Serializable]
+	public class VPViewRequest
+	{
+		public int RequestId { get; set; }
+		public DateTime? StartDate { get; set; }
+		public DateTime? EndDate { get; set; }      
+        public string TypesofLeaveAvailable { get; set; }
+        public string WeeksAvailableAnnually { get; set; }
+        public Decimal? PercentageofWagesPaid { get; set; }
+        public string DocumentName { get; set; }
+    }
+}
